ff751948d14ecb5381d05545d6c3a838d0aff330
Shell <galishnikov.ilya@yandex.ru>
2024-05-07 17:08:25 +0300

    1

--- src/comparator.py
+++ src/comparator.py
@@ -1,14 +1,19 @@
 from src.model import Transit, RfidDetection
 
-
 def compare_transits_and_rfid_detections(
-    transits: list(Transit),
-    detections: list(RfidDetection)
-):
-    """"
-        Реализовать алгоритм сравнения, в котором каждому номеру из списка detections
-        сопоставляется проезд, зафикисрованный с помощью камеры.
-        Подробно алгоритм сопоставления описан в документе, высланном ранее
+    transits: list[Transit],
+    detections: list[RfidDetection]
+) -> int:
+    matched_numbers = set()
 
-    """
-    pass
+    for detection in detections:
+        if detection.rfid_num is not None:
+
+            for transit in transits:
+                if transit.num == detection.rfid_num:
+                    matched_numbers.add(transit.num)
+                    break
+
+    unmatched_count = len(detections) - len(matched_numbers)
+    return unmatched_count
+pass
