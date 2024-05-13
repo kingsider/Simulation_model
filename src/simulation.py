58b527098cc94a423b66c1af8d3a242de1dbf4f4
aleksandrsokolov <timac11@bk.ru>
2024-05-13 16:06:46 +0300

    updates

--- src/simulation.py
+++ src/simulation.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List, Tuple, Optional
 
-from src.generator import generate_plate, generate_transit
+from src.generator import generate_plate, generate_transit, generate_rfid_detection
 from src.model import Transit, ModelParams, RfidDetection
 from heapq import heappush, heappop
 
@@ -15,8 +15,8 @@ class _Event(Enum):
 
 
 _EventQueue = List[Tuple[float, _Event, Optional[int]]]
-_Transits = List[Tuple[int, Transit]]
-_RfidDetections = List[Tuple[int, RfidDetection]]
+_Transits = List[Tuple[Transit, int]]
+_RfidDetections = List[Tuple[RfidDetection, int]]
 
 
 def simulate(
@@ -40,7 +40,7 @@ def simulate(
     cur_transits = 0
 
     _queue: _EventQueue = []  # use heappush and heappop
-    transits: _Transits = []
+    transits: _Transits = [] # use transits
     rfid_detections: _RfidDetections = []
 
     # init model
@@ -63,9 +63,9 @@ def simulate(
 
     while _queue:
         __cur_time, __cur_event, __num_id = heappop(_queue)
+        cur_time = __cur_time
 
         if __cur_event == _Event.PHOTO_TRANSIT:
-            cur_time = __cur_time
             transit = generate_transit(
                 cur_time=cur_time,
                 sign_prob=sign_prob,
@@ -75,39 +75,71 @@ def simulate(
                 photo_error=photo_error,
                 car_error=car_error
             )
-            _Transits.append(transit)
+            transits.append((transit, cur_transits))
+
+            # event for rfid detection of this transit
+            __push_rfid_detection_event(
+                _queue=_queue,
+                cur_time=cur_time,
+                transit=transit,
+                rfid_distance=rfid_distance,
+                cur_transits=cur_transits
+            )
+
+            cur_transits += 1
+
+            # event for next transit
+
+            __push_photo_event(
+                _queue=_queue,
+                cur_time=cur_time,
+                speed=speed,
+                distance_between_transports=distance_between_transports,
+                cur_transits=cur_transits
+            )
         elif __cur_event == _Event.RFID_DETECTION:
-            pass
+            matches = [_transit for _transit in transits if _transit[0] == __num_id]
+
+            if len(matches) == 0:
+                raise Exception(f'Invalid num id: {__num_id}')
+
+            cur_detection = generate_rfid_detection(
+                time=cur_time,
+                transit=matches[0],
+                rfid_error=rfid_error,
+            )
+
+            rfid_detections.append((cur_detection, __num_id))
         elif __cur_event == _Event.START_MERGE:
-            # start merging and add
+            # start merging transits with rfid detections
             pass
         else:
             raise Exception('Invalid event type')
 
 
 def __get_photo_time(
-    cur_time: float,
-    speed: float,
-    distance_between_transports: float
+        cur_time: float,
+        speed: float,
+        distance_between_transports: float
 ) -> float:
     return cur_time + distance_between_transports / speed
 
 
 def __get_rfid_detection_time(
-    cur_time: float,
-    speed: float,
-    photo_distance: float,
-    rfid_distance: [float, float],
+        cur_time: float,
+        speed: float,
+        photo_distance: float,
+        rfid_distance: [float, float],
 ) -> float:
     return cur_time + photo_distance - (np.random.uniform(low=rfid_distance[0], high=rfid_distance[1])) / speed
 
 
 def __push_rfid_detection_event(
-    _queue: _EventQueue,
-    cur_time: float,
-    transit: Transit,
-    rfid_distance: [float, float],
-    cur_transits: int
+        _queue: _EventQueue,
+        cur_time: float,
+        transit: Transit,
+        rfid_distance: [float, float],
+        cur_transits: int
 ):
     heappush(_queue, (
         __get_rfid_detection_time(
@@ -122,11 +154,11 @@ def __push_rfid_detection_event(
 
 
 def __push_photo_event(
-    _queue: _EventQueue,
-    cur_time: float,
-    speed: list[float, float],
-    distance_between_transports: float,
-    cur_transits: int
+        _queue: _EventQueue,
+        cur_time: float,
+        speed: list[float, float],
+        distance_between_transports: float,
+        cur_transits: int
 ):
     heappush(_queue, (
         __get_photo_time(
@@ -140,14 +172,14 @@ def __push_photo_event(
 
 
 def __push_merge_event(
-    _queue: _EventQueue,
-    cur_time: float,
-    speed: list[float, float],
-    photo_distance: [float, float],
-    rfid_distance: [float, float],
+        _queue: _EventQueue,
+        cur_time: float,
+        speed: list[float, float],
+        photo_distance: [float, float],
+        rfid_distance: [float, float],
 ):
     heappush(_queue, (
         cur_time + (speed[0] + speed[1]) / (photo_distance[0] + photo_distance[1] - rfid_distance[1]),
         _Event.START_MERGE,
         None)
-    )
+             )
